ef29ccf65d503c57e640e383d842b09bc864b856
System <Mr.NN91@gmail.com>
2017-01-14 22:17:49 +0700

    Create install.evm.sh

---
+++ evm/install.evm.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+
+
+
+
+# environment-messaging
+
+yum -y install rabbitmq-server
+
+#
+systemctl enable rabbitmq-server
+systemctl start rabbitmq-server
+
+
+# Add the openstack user:
+rabbitmqctl add_user openstack RABBIT_PASS
+
+
+# Permit configuration, write, and read access for the openstack user:
+rabbitmqctl set_permissions openstack ".*" ".*" ".*"
+
+
+
+
+
+
+
+
+
