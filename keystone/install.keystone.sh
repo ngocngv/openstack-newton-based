75f755d317f414afc73940a23ce35e1eebb4f55c
System <Mr.NN91@gmail.com>
2017-01-14 22:31:57 +0700

    Update install.keystone.sh

--- keystone/install.keystone.sh
+++ keystone/install.keystone.sh
@@ -1,1 +1,32 @@
+#!/usr/bin/bash
 
+
+
+#
+yum remove -y MariaDB-shared
+yum remove -y MariaDB-common
+
+#
+yum -y install mariadb mariadb-server MySQL-python
+yum -y install mariadb mariadb-server python2-PyMySQL
+
+
+
+mysql -u root -p
+
+CREATE DATABASE keystone;
+GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'localhost' IDENTIFIED BY 'KEYSTONE_DBPASS';
+GRANT ALL PRIVILEGES ON keystone.* TO 'keystone'@'%' IDENTIFIED BY 'KEYSTONE_DBPASS';
+  
+  
+  
+  
+yum -y install openstack-keystone httpd mod_wsgi
+  
+  
+  
+  
+  
+  
+  
+  
