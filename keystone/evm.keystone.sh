d5091cbe5eda823a242f9c635876c009c6a641e3
System <Mr.NN91@gmail.com>
2017-01-17 10:47:27 +0700

    Update evm.keystone.sh

--- keystone/evm.keystone.sh
+++ keystone/evm.keystone.sh
@@ -1,1 +1,34 @@
+#!/bin/bash
+
+
+
+
+# Create the service project:
+openstack project create --domain default --description "Service Project" service
+
+# Create the demo project:
+openstack project create --domain default --description "Demo Project" demo
+
+
+# Create the demo user:
+#openstack user create --domain default --password-prompt demo
+openstack user create demo --domain default --password demo
+
+
+# Create the user role:
+openstack role create user
+
+# Add the user role to the demo project and user:
+openstack role add --project demo --user demo user
+
+
+
+
+
+
+
+
+
+
+
 
