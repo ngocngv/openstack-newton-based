8b583ad2f5255e8fa25b9b7d7a06ae9fd8c70d37
System <Mr.NN91@gmail.com>
2017-01-17 14:44:51 +0700

    Update verify.keystone.sh

--- keystone/verify.keystone.sh
+++ keystone/verify.keystone.sh
@@ -5,11 +5,16 @@
 
 
 # As the admin user, request an authentication token:
+openstack --os-auth-url http://ctl01:35357/v3 \
+    --os-project-domain-name Default --os-user-domain-name Default \
+    --os-project-name admin --os-username admin token issue
 
 
 
-
-
+# As the demo user, request an authentication token:
+openstack --os-auth-url http://ctl01:5000/v3 \
+    --os-project-domain-name Default --os-user-domain-name Default \
+    --os-project-name demo --os-username demo token issue
 
 
 
